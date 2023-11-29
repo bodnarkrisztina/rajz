f253a81b558f2cdfd0c82a433c671cf337e84880
Kriszta <90178422+bodnarkrisztina@users.noreply.github.com>
2023-11-29 14:36:24 +0100

    Update rajz.py

--- rajz.py
+++ rajz.py
@@ -8,15 +8,10 @@ fig = px.line(x=[1], y=[1])
 selected_points = plotly_events(fig)
 
 # Can write inside of things using with!
-with st.beta_expander('Plot'):
 with st.expander('Plot'):
     fig = px.line(x=[1], y=[1])
     selected_points = plotly_events(fig)
 
-@@ -36,7 +38,7 @@ Returns
-list of dict
-    List of dictionaries containing point details (in case multiple overlapping points have been clicked).
-    Details can be found here: 
-    Details can be found here:
-        https://plotly.com/javascript/plotlyjs-events/#event-data
-    Format of dict:
+# Select other Plotly events by specifying kwargs
+fig = px.line(x=[1], y=[1])
+selected_points = plotly_events(fig, click_event=False, hover_event=True)
